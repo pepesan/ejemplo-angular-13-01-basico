38b8f684cb8680bda66a47beadf0d2cabe6accb0
David Vaquero <pepesan@gmail.com>
2022-01-17 19:29:06 +0100

    mejorados los test de la app base

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'nombre_proyecto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nombre_proyecto');
+    expect('mixins').toEqual(app.title);
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nombre_proyecto app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mixins app is valor! Cuarto Milenio');
   });
 });
