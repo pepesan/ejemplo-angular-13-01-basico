1f1795dcb87a4e72e6d9a80bc3dc123d0ab274bf
David Vaquero <pepesan@gmail.com>
2022-01-17 19:14:45 +0100

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nombre_proyecto';
+  title = 'mixins';
+  cadena = 'valor';
 }
