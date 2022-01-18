427ff5d2d79f72210da1e0f15ab050c86d7bc5df
David Vaquero <pepesan@gmail.com>
2022-01-18 15:54:39 +0100

    mejora el ejemplo de formulario chiquito

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+  todos: any[]= [
+    {text:"hacer la compra"},
+    {text: "revisar el coche"}
+  ];
+  todo: any = {text: ""};
+
   constructor() { }
+
+
+
+  addTodo() {
+    //console.log(this);
+    let mitodo={text:this.todo.text};
+    this.todos?.push(mitodo);
+    this.todo= {text: ""};
+  }
 
   ngOnInit(): void {
   }
-
 }
