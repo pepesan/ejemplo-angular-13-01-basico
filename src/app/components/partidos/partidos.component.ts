c41479be3871cae9ceeba4d5fdd8536010580499
David Vaquero <pepesan@gmail.com>
2022-01-19 15:54:38 +0100

    Revert "chungo"
    This reverts commit fa1820a919ab20e38b6fde700f81dc03452c617c.

--- src/app/components/partidos/partidos.component.ts
+++ src/app/components/partidos/partidos.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PartidosService} from "../../services/partidos.service";
 import {Partido} from "./partido";
-import {Observable} from "rxjs";
-import {valueReferenceToExpression} from "@angular/compiler-cli/src/ngtsc/annotations/src/util";
 
 @Component({
   selector: 'app-partidos',
@@ -12,32 +10,12 @@ import {valueReferenceToExpression} from "@angular/compiler-cli/src/ngtsc/annota
 export class PartidosComponent implements OnInit {
 
   partidos: Promise<Partido[]>;
-  partidosVisibles: Partido[];
-  partidosObservable: Observable<Partido[]>;
-  constructor(private _partidosService: PartidosService) {
-    // pido datos y paso a vista
-    this.partidos = this._partidosService.getData().toPromise();
-    // variable de vista "vacía"
-    this.partidosVisibles = [];
-    // LLamada a petición para devolver un observable
-    this.partidosObservable = this._partidosService.getData();
 
+  constructor(private _partidosService: PartidosService) {
+    this.partidos = this._partidosService.getData().toPromise();
   }
 
   ngOnInit(): void {
-    // subscribiendome al observable para procesar los datos y pasarlos a la vista
-    this.partidosObservable.subscribe((data) => {
-      console.log(data);
-      console.log(data.length);
-
-      data.forEach( (value : Partido) => {
-        //console.log(value);
-        //let partido: Partido = new Partido(value.nombre, value.dipu, value.imagen);
-        this.partidosVisibles.push(value);
-      });
-
-      console.log(this.partidosVisibles);
-    });
   }
 
 }
