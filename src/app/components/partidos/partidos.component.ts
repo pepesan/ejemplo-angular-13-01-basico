4f74b04aa26adea47155d4e1be99226ca0887d20
David Vaquero <pepesan@gmail.com>
2022-01-19 15:56:57 +0100

    commit bueno de uso de subscribe

--- src/app/components/partidos/partidos.component.ts
+++ src/app/components/partidos/partidos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {PartidosService} from "../../services/partidos.service";
 import {Partido} from "./partido";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-partidos',
@@ -10,12 +11,32 @@ import {Partido} from "./partido";
 export class PartidosComponent implements OnInit {
 
   partidos: Promise<Partido[]>;
-
+  partidosVisibles: Partido[];
+  partidosObservable: Observable<Partido[]>;
   constructor(private _partidosService: PartidosService) {
+    // pido datos y paso a vista
     this.partidos = this._partidosService.getData().toPromise();
+    // variable de vista "vacía"
+    this.partidosVisibles = [];
+    // LLamada a petición para devolver un observable
+    this.partidosObservable = this._partidosService.getData();
+
   }
 
   ngOnInit(): void {
+    // subscribiendome al observable para procesar los datos y pasarlos a la vista
+    this.partidosObservable.subscribe((data) => {
+      console.log(data);
+      console.log(data.length);
+
+      data.forEach( (value : Partido) => {
+        //console.log(value);
+        //let partido: Partido = new Partido(value.nombre, value.dipu, value.imagen);
+        this.partidosVisibles.push(value);
+      });
+
+      console.log(this.partidosVisibles);
+    });
   }
 
 }
